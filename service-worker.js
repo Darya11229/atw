4d8dcad6d18a2f3d889f1e6fa6799c4124ca7f3c
Darya11229 <darya2006sh@yandex.ru>
2025-07-17 16:33:14 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,27 @@
 const CACHE_NAME = 'atw-cache-v1';
 const ASSETS_TO_CACHE = [
+  '/atw/11.html',
+  '/atw/22.css',
+  '/atw/pl1.html',
+  '/atw/eks1.html',
+  '/atw/act1.html',
+  '/atw/feedback1.html',
+  '/atw/эк1.jpg',
+  '/atw/экс2(1).jpg',
+  '/atw/экс3.jpg',
+  '/atw/пл(1).jpg',
+  '/atw/пл2.jpg',
+  '/atw/пл3.jpg',
+  '/atw/ак1.jpg',
+  '/atw/ак2.jpg',
+  '/atw/ак3.jpg',
+  '/atw/icon-192.png',
+  '/atw/icon-512.png',
+  '/atw/app.js'
+];
+
+const CACHE_NAME = 'atw-cache-v1';
+const ASSETS_TO_CACHE = [
   '/11.html',
   '/22.css',
   '/pl1.html',
